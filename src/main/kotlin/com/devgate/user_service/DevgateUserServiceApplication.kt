238a989bd3502cb16aa292520277face68fd46ff
pr0gger1 <pr0gger.dev@gmail.com>
2026-01-20 13:41:12 +0300

    init commit

---
+++ src/main/kotlin/com/devgate/user_service/DevgateUserServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.devgate.user_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DevgateUserServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DevgateUserServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/devgate/user_service/DevgateUserServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devgate.user_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevgateUserServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

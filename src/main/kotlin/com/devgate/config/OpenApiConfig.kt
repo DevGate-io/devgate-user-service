a6ae5110c2f0ffce72f2ec893eb92d1b0efa45cc
pr0gger1 <pr0gger.dev@gmail.com>
2026-06-21 13:26:55 +0300

    feat(openapi): конфигурация OpenAPI (bearer-JWT схема и метаданные API)

---
+++ src/main/kotlin/com/devgate/config/OpenApiConfig.kt
@@ -0,0 +1,38 @@
+package com.devgate.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiConfig {
+
+	@Bean
+	fun openApi(): OpenAPI {
+		return OpenAPI()
+			.info(
+				Info()
+					.title("DevGate User Service API")
+					.description("User management and authentication (JWT access token + refresh-token cookie)")
+					.version("1.0.0")
+			)
+			.components(
+				Components().addSecuritySchemes(
+					BEARER_SCHEME,
+					SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")
+				)
+			)
+			.addSecurityItem(SecurityRequirement().addList(BEARER_SCHEME))
+	}
+
+	private companion object {
+		const val BEARER_SCHEME = "bearer-jwt"
+	}
+}

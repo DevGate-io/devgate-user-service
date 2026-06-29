cee8fa8dbd51c6a639cc9066509c47aebaae8c14
pr0gger1 <pr0gger.dev@gmail.com>
2026-06-30 01:32:20 +0300

    fix: возвращать 401 вместо 200 в JwtAuthenticationEntryPoint

--- src/main/kotlin/com/devgate/auth/security/JwtAuthenticationEntryPoint.kt
+++ src/main/kotlin/com/devgate/auth/security/JwtAuthenticationEntryPoint.kt
@@ -2,19 +2,18 @@ package com.devgate.auth.security
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED
 import org.springframework.security.core.AuthenticationException
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.stereotype.Component
 
 @Component
-class JwtAuthenticationEntryPoint: AuthenticationEntryPoint {
+class JwtAuthenticationEntryPoint : AuthenticationEntryPoint {
 	override fun commence(
 		request: HttpServletRequest,
 		response: HttpServletResponse,
 		authException: AuthenticationException
 	) {
-		val status = response.status
-
-		response.sendError(status, authException.message)
+		response.sendError(SC_UNAUTHORIZED, authException.message)
 	}
 }
